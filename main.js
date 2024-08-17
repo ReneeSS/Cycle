8fe1a1c0ccabb83c4f9bc51c3b94eb7afcf93d6d
ReneeSS <reneeffs@gmail.com>
2024-08-17 17:17:29 +0300

    for in for

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ for(let i = 1; i <= 10; i++){
   }
   console.log(`${i} итеррация`);
 }
+
+//Цыкл внутри цыкла!
+
+for (let i = 0; i < 3; i++){
+  console.log(`${i} i layer`)
+  for (let j = 0; j < 3; j++){
+    console.log(`${j} j layer`)
+  }
+}
+
+let result = "";
+const length = 7;
+
+for (let i = 1; i < length; i++){
+  for(let j = 0; j < i; j++){
+    result += "*"
+  }
+
+  result += "\n"
+}
+
+console.log(result);
+
